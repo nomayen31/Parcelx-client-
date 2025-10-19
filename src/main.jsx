9e58897fe303c21fe6720dcd7b52e14db519f113
nomayen31 <nomayen.ohin@gmail.com>
2025-10-19 17:16:01 +0600

    context , auth , and firebase setuo

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
 import "./index.css";
+import AuthProvider from "./Contexts/AuthContext/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="font-urbanist max-w-[1850px] mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </React.StrictMode>
 );
